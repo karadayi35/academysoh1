43f68254195b3c96fc0fc8ac1599b606b79194b5
karadayi35 <aalikirmizigul89@gmail.com>
2024-10-19 23:41:15 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient, events
+from telethon.errors import FloodWaitError
 import asyncio
 import re
 
@@ -8,44 +9,45 @@ api_hash = '6611d0556f6f8dc7b9190803cc442dec'  # Telegram API Hash
 
 # Kullanacağınız hesapların telefon numaraları ve session isimleri
 accounts = [
-    ('+447599168809', 'session_1'),
-    ('+447957201395', 'session_2'),
-    ('+447903319837', 'session_4'),
-    ('+447879090554', 'session_5'),
-    ('+447393306945', 'session_6'),
-    ('+447901829995', 'session_7'),
-    ('+447555132839', 'session_8'),
-    ('+447887221883', 'session_9'),
-    ('+46760749893', 'session_10'),
-    ('+353873812868', 'session_11'),
-    ('+85270945200', 'session_12'),
-    ('+37367713916', 'session_13'),
-    ('+447469301950', 'session_14'),
-    ('+447562118006', 'session_15'),
-    ('+447856096750', 'session_16'),
-    ('+447517007599', 'session_17'),
-    ('+447926762107', 'session_18'),
-    ('+447563091691', 'session_19'),
-    ('+447939686582', 'session_20'),
-    ('+447961267689', 'session_21'),
-    ('+447957613256', 'session_22'),
-    ('+447538366715', 'session_24'),
-    ('+447930877620', 'session_25'),
-    ('+447932346011', 'session_26'),
-    ('+447985132321', 'session_27'),
-    ('+447908575748', 'session_28'),
-    ('+447517007674', 'session_29'),
-    ('+447745161785', 'session_30'),
-    ('+447742588629', 'session_31'),
-    ('+447752559964', 'session_32'),
-    ('+447729389626', 'session_33'),
-    ('+447599168979', 'session_34'),
-    ('+447801101779', 'session_35'),
-    ('+447907325741', 'session_36'),
-    ('+447951829503', 'session_44'),
-    ('+447563041929', 'session_45'),
-    ('+447763916635', 'session_47'),
-    ('+447932516781', 'session_48'),
+   ('+447599168809 ', 'session_1'),
+    ('+447957201395 ', 'session_2'),
+    ('+447903319837 ', 'session_4'),
+    ('+447879090554 ', 'session_5'),
+    ('+447393306945 ', 'session_6'),
+    ('+447901829995 ', 'session_7'),
+    ('+447555132839 ', 'session_8'),
+    ('+447887221883 ', 'session_9'),
+    ('+46760749893 ', 'session_10'),
+    ('+353873812868 ', 'session_11'),
+    ('+85270945200 ', 'session_12'),
+    ('+37367713916 ', 'session_13'),
+    ('+447469301950 ', 'session_14'),
+    ('+447562118006 ', 'session_15'),
+    ('+447856096750 ', 'session_16'),
+    ('+447517007599  ', 'session_17'),
+    ('+447926762107 ', 'session_18'),
+    ('+447563091691 ', 'session_19'),
+    ('+447939686582 ', 'session_20'),
+    ('+447961267689 ', 'session_21'),
+    ('+447957613256  ', 'session_22'),
+
+    ('+447538366715 ', 'session_24'),
+    ('+447930877620 ', 'session_25'),
+    ('+447932346011 ', 'session_26'),
+    ('+447985132321  ', 'session_27'),
+    ('+447908575748 ', 'session_28'),
+    ('+447517007674 ', 'session_29'),
+    ('+447745161785 ', 'session_30'),
+    ('+447742588629 ', 'session_31'),
+    ('+447752559964 ', 'session_32'),
+    ('+447729389626 ', 'session_33'),
+    ('+447599168979 ', 'session_34'),
+    ('+447801101779 ', 'session_35'),
+    ('+447907325741 ', 'session_36'),
+    ('+447951829503  ', 'session_44'),
+    ('+447563041929  ', 'session_45'),
+    ('+447763916635  ', 'session_47'),
+    ('+447932516781  ', 'session_48'),
 ]
 
 # Kaynak gruplar ve hedef grup
@@ -94,12 +96,17 @@ async def forward_messages(clients):
                     # Mesajı hedef gruba gönderirken farklı hesaplar arasında döngü yap
                     await clients[client_index].send_message(target_group, message.text)
 
-                    # Her mesaj için bir sonraki hesabı kullan
+                    # Hesapları sırayla döndür
                     client_index = (client_index + 1) % len(clients)
+
+                except FloodWaitError as e:
+                    # Eğer bir flood wait hatası varsa, belirtilen süre boyunca bekle
+                    print(f"Flood wait error: {e.seconds} saniye bekleniyor...")
+                    await asyncio.sleep(e.seconds)
 
                 except Exception as e:
                     print(f"Hata oluştu: {e}")
-                    # Eğer bir hata varsa, sıradaki hesaba geç
+                    # Diğer hatalarda sıradaki hesaba geç
                     client_index = (client_index + 1) % len(clients)
 
         print(f"Mesajlar {source_group} grubundan çekilmeye başlandı...")
